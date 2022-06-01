393a7e99d2c67a76e80350de05648c69a7bc5d34
gomdol <oioifor@naver.com>
2022-06-02 02:03:24 +0900

    2022-06-01 네트워크,게임 개발

---
+++ bear1230/all_Programmers/Network.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] computers) {  
+        int answer = 0;   
+        boolean[] visited = new boolean[n];   
+        Queue<int[]> que = new LinkedList<>();
+    
+        for(int i=0; i<computers.length; i++) {
+            if(visited[i]) continue;
+            que.offer(computers[i]);
+            visited[i] = true;
+            
+            while(!que.isEmpty()) {
+                int[] computer = que.poll();
+                for(int j=0; j<computer.length; j++) {          
+                    if(!visited[j] && computer[j] == 1) {                  
+                        que.offer(computers[j]);                  
+                        visited[j] = true;            
+                    }           
+                }
+            }
+       
+            answer++;
+  
+        }
+        return answer;
+
+    }
+}
