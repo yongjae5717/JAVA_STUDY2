83ea59dbc99610dd6c495745f232884aeb4f92a8
hobin <elwlahd555@naver.com>
2022-06-22 19:22:09 +0900

    2022-06-22 우수 마을, 연료 채우기, 타일 채우기, 양팔 저울

---
+++ elwlahd555/baekjoon/baekjoon2133_타일_채우기.java
@@ -0,0 +1,32 @@
+package elwlahd555.baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon2133_타일_채우기 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        int answer = 0;
+        if (n % 2 == 1) {
+            answer = 0;
+        } else {
+            dp[0] = 1;
+            dp[2] = 3;
+
+            for (int i = 4; i <= n; i += 2) {
+                dp[i] = dp[i - 2] * dp[2];
+                for (int j = i - 4; j >= 0; j -= 2) {
+                    dp[i] += dp[j] * 2;
+                }
+            }
+
+            answer = dp[n];
+        }
+
+        System.out.println(answer);
+        sc.close();
+    }
+}
