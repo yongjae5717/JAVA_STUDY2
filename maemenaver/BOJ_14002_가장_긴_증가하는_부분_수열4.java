28e70d30e21da730e0a7a3af170cdf4e776f0348
Jeonghun Ha <maemenaver@gmail.com>
2022-06-30 01:23:45 +0900

    2022-06-30 가장 긴 증가하는 부분 수열 4 (실패)

---
+++ maemenaver/BOJ_14002_가장_긴_증가하는_부분_수열4.java
@@ -0,0 +1,28 @@
+package maemenaver;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_14002_가장_긴_증가하는_부분_수열4 {
+	public static void main(String[] args)
+			throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		List<Integer> p = new ArrayList<Integer>();
+		
+		for (int i = 0; i < N; i++) {
+			p.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		bw.write(Integer.toString(4));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
