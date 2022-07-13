0c7aa6f5fc99e7a40149cc48c7b23b43e79a7558
Jeonghun Ha <maemenaver@gmail.com>
2022-07-14 00:28:01 +0900

    ✅ 2022-07-14 게임

--- maemenaver/BOJ_1103_게임.java
+++ maemenaver/BOJ_1103_게임.java
@@ -3,18 +3,24 @@ import java.util.*;
 
 class BoardElement {
   int element;
-  int maxMove = 0;
+  // int maxMove = 0;
+  int maxMove = -1;
   BoardElement[] next = new BoardElement[4];
   
   BoardElement(int e) {
     this.element = e;
   }
 
-  int deep(int _move, ArrayList<BoardElement> checked) {  
+  // int deep(int _move, ArrayList<BoardElement> checked) {  
+  int deep(ArrayList<BoardElement> checked) {  
     checked.add(this);
-    boolean isNext = false;
+    // boolean isNext = false;
 
-    int _max = _move;
+    // int _max = _move;
+    int _max = 0;
+    if (this.maxMove != -1) {
+      return maxMove;
+    }
     for (int i = 0 ; i < 4 ; i++) {
       var nextBoard = this.next[i];
       if (nextBoard == null) {
@@ -28,33 +34,35 @@ class BoardElement {
 
       // 다녀간 곳이면
       if (checked.contains(nextBoard)) {
-        return -1;
+        return 1000000000;
       }
 
       // 그 외의 경우
       int result = 0;
-      
-      if (nextBoard.maxMove == 0) {
-        result = nextBoard.deep(_move+1, (ArrayList<BoardElement>) checked.clone());
-      } else {
-        result = nextBoard.maxMove;
-      }
 
-      if (result == -1) {
-        return -1;
-      }
+      result = nextBoard.deep((ArrayList<BoardElement>) checked.clone()) + 1;
+      
+      // if (nextBoard.maxMove == 0) {
+      //   result = nextBoard.deep(_move+1, (ArrayList<BoardElement>) checked.clone());
+      // } else {
+      //   result = nextBoard.maxMove;
+      // }
+
+      // if (result == -1) {
+      //   return -1;
+      // }
       
       _max = Math.max(result, _max);
-      isNext = true;
+      // isNext = true;
     }
 
-    if (!isNext) {
-      maxMove = Math.max(maxMove, _move+1);
-      return _move+1;
-    }
+    // if (!isNext) {
+    //   maxMove = Math.max(maxMove, _move+1);
+    //   return maxMove;
+    // }
 
     maxMove = Math.max(maxMove, _max);
-    return _max;
+    return maxMove;
   }
 }
 
@@ -101,7 +109,12 @@ public class Main {
     if (board[0][0].element == 0) {
       System.out.println(0);
     } else {
-      System.out.println(board[0][0].deep(0, new ArrayList<BoardElement>()));
+      int s = board[0][0].deep(new ArrayList<BoardElement>());
+      if (s >= 1000000000) {
+        System.out.println(-1);
+      } else {
+        System.out.println(s+1);
+      }
     }
   }
 }
