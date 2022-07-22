e6845a03d73836ab5e6737844f2043c6e74e12e7
hobin <elwlahd555@naver.com>
2022-07-22 20:27:07 +0900

    2022-07-22 문제 출제

--- elwlahd555/baekjoon/baekjoon1480_보석_모으기.java
+++ elwlahd555/baekjoon/baekjoon1480_보석_모으기.java
@@ -1,5 +1,10 @@
 package elwlahd555.baekjoon;
 
-public class baekjoon1480_보석_모으기 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class baekjoon1480_보석_모으기 {
+	public static void main(String[] args) {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	}
 }
