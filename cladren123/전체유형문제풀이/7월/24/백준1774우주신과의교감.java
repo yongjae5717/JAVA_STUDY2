c9f27ec2f2cc32910f52e80657a1691cc0f89afc
cladren123 <48821942+cladren123@users.noreply.github.com>
2022-07-26 20:28:01 +0900

    2022-07-26 우주신과의 교감

---
+++ cladren123/전체유형문제풀이/7월/24/백준1774우주신과의교감.java
@@ -0,0 +1,134 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/*
+
+크루스칼 알고리즘 으로해결
+ */
+
+class Point {
+    int num;
+    double x;
+    double y;
+
+    Point(int num, double x, double y) {
+        this.num = num;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Edge implements Comparable<Edge> {
+    int start;
+    int end;
+    double weight;
+
+    Edge(int start, int end, double weight) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        if (weight < o.weight) {
+            return -1;
+        }
+        return 1;
+    }
+
+}
+
+public class 백준1774우주신과의교감 {
+    static int[] parent;
+    static ArrayList<Edge> edgeList;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        Point[] points = new Point[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            double x = Double.parseDouble(st.nextToken());
+            double y = Double.parseDouble(st.nextToken());
+
+            points[i] = new Point(i, x, y);
+        }
+
+        // 사전에 연결된 간선을 미리 합집합 연산함.
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            union(start, end);
+        }
+
+        // 모든 통로들 간의 간선과 비용 정보를 edgeList에 넣어 둔다.
+        edgeList = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                double weight = distance(points[i], points[j]);
+
+                edgeList.add(new Edge(points[i].num, points[j].num, weight));
+            }
+        }
+        Collections.sort(edgeList); // 간선의 비용을 기준으로 오름차순 정렬.
+
+        double ans = 0;
+
+        // 크루스칼 알고리즘 수행.
+        for (int i = 0; i < edgeList.size(); i++) {
+            Edge edge = edgeList.get(i);
+
+            if (find(edge.start) != find(edge.end)) {
+                ans += edge.weight;
+                union(edge.start, edge.end);
+            }
+        }
+
+        bw.write(String.format("%.2f", ans) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+}
