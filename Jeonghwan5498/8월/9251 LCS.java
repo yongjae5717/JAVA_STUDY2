d1ff07fd6b9c0bae0dc0bd8a7c1eb556839b8b90
kohj5078 <hjhj5077@gmail.com>
2022-08-29 17:07:21 +0900

    2022-08-29 LCS
    2022-08-29 LCS

---
+++ Jeonghwan5498/8월/9251 LCS.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // System.setIn(new FileInputStream("src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+        int[][] dp = new int[b.length()+1][a.length()+1];
+
+
+        for(int i = 1; i <= b.length(); i++){
+
+            for(int j = 1; j <= a.length(); j++){
+
+                if(a.charAt(j-1) == b.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        System.out.println(dp[b.length()][a.length()]);
+
+    }
+}
+
+
+
